b78ef7b58b968d4b88c74efcb74ea785c7f2385d
SKalt <kalt.steven@gmail.com>
2017-09-06 16:25:05 -0400

    Completed a draft of the iframe-embeddable map

--- src/map.js
+++ src/map.js
@@ -1,27 +1,84 @@
-(function(d) {
-  function inject(tag, opts, onload){
-    element = Object.assign(d.createElement(tag), opts);
-    if (onload) element.onload = onload;
-    d.getElementsByTagName('head')[0].appendChild(element)
-  }
-  async function promiseInject(tag, opts){
-    return new Promise((resolve) => inject(tag, opts, resolve));
-  };
-  async function injectLeaflet(){
-    return promiseInject('link', {
-      rel:"stylesheet", href:"https://unpkg.com/leaflet@1.2.0/dist/leaflet.css",
-      integrity:"sha512-M2wvCLH6DSRazYeZRIm1JnYyh22purTM+FDB5CsyxtQJYeKq83arPe5wgbNmcFXGqiSH2XR8dT/fJISVA1r/zQ=="
-      crossorigin:""
-    }).then(()=>promiseInject('script', {
-      src:"https://unpkg.com/leaflet@1.2.0/dist/leaflet.js",
-      integrity:"sha512-lInM/apFSqyy1o6s89K4iQUKg6ppXEgsVxT35HbzUupEVRh2Eu9Wdl4tHj7dZO0s1uvplcYGmt3498TtHq+log=="
-      crossorigin:""
-    }));
-  }
-  function __init__(){
-    L.map(MAP_ID).setview(VIEW);
-    L.geoJSON(myLines, { //TODO: import from gsheets
-    style: myStyle
-}).addTo(map);
-  }
-}(document));
+import queryString from 'query-string';
+import {point, featureCollection} from '@turf/helpers';
+/**
+ * Maps rows in a google sheet to GeoJSON points
+ * @param  {String[]} colNames An array of column names
+ * @param  {Array<String>|Array<Number>} row a row from the imported google sheet
+ * @return {GeoJSON.Point[]} an array of GeoJSON points with properties and coordinates from their row.
+ */
+const toPoints = (colNames, row)=>{
+  let properties = colNames.map((col, index) => {
+      return {[col || index]: row[index]}
+    })
+    .reduce(Object.assign, {});
+  let [lat, lng] = [x, y].map(colName => {
+    coord = properties[colName];
+    delete properties[colName];
+    return coord;
+  });
+  return point([lng, lat], properties);
+}
+/**
+ * Maps an array of rows to a GeoJSON FeatureCollection of Points.
+ * @param  {Array<Array>} json the array of rows in the imported google sheet.
+ * @return {GeoJSON.FeatureCollection} a collection of point features.
+ */
+const toGeoJson = json => {
+    let values = json.values || json;
+    if (!Array.isArray(values) || !values.length) throw new Error('No values');
+    let colNames = values.splice(headerRow || 0, 1);
+    return featureCollection(values.map((row)=>toPoints(colNames, row)));
+};
+/**
+ * Turns a point's properties into a string of property : value lines.
+ * @param  {Object} properties a plain object
+ * @return {String}  a string of property : value lines.
+ */
+const propertyTable = (properties)=> Object.entries(properties)
+  .map(entry => entry.join(' : '))
+  .join('\n');
+
+let {
+  apiKey,
+  sheetId,
+  range,
+  x, y, headerRow
+  lat, lng, zoom,
+  color, weight, opacity, radius
+} = queryString.parse(location.query);
+// override defaults with queryString parameters
+let style = {
+  radius: radius || 8,
+  fillColor: color || "#ff7800",
+  color: "#000",
+  weight: 1,
+  opacity: 1,
+  fillOpacity: opacity || 0.8
+};
+
+const pointToLayer = (feature, latLng) => L.circleMarker(latlng, style);
+/**
+ * binds a tooltip to each point
+ */
+function onEachFeature(feature, layer){
+  return layer.bindPopup(propertyTable(feature.properties));
+}
+try {
+  L.map('map').setView([lat, lng], zoom);
+  let url = 'https://sheets.googleapis.com/v4/spreadsheets/' +
+    `${sheetId}/values/{range}?key=${apiKey}`;
+  fetch(url).then(response => {
+    if (!response.ok){
+      throw new Error(`response status ${response.status}`);
+    }
+    return response.json();
+  }).then(
+    points => L.geoJson(points, {
+      onEachFeature,
+      pointToLayer
+    }).addTo(map);
+  });
+} catch(err){
+  alert('API key invalid on this domain');
+  throw err;
+}
